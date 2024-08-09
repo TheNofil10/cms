f1cd69768ed9da00df8385b5c3896ef0c5171633
musxeto <mustafamalikawan786@gmail.com>
2024-08-09 10:24:04 +0500

    updated api

--- company_management_system/employee_management/urls.py
+++ company_management_system/employee_management/urls.py
@@ -8,9 +8,9 @@ router.register(r'employees', EmployeeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('admin/employees/', AdminEmployeeView.as_view({'get': 'list'}), name='admin_employee_list'),
-    path('admin/employees/create/', AdminEmployeeView.as_view({'post': 'create'}), name='admin_employee_create'),
-    path('admin/employees/<pk>/', AdminEmployeeView.as_view({'delete': 'destroy'}), name='admin_employee_delete'),
+    path('employees/', AdminEmployeeView.as_view({'get': 'list'}), name='admin_employee_list'),
+    path('employees/create/', AdminEmployeeView.as_view({'post': 'create'}), name='admin_employee_create'),
+    path('employees/<pk>/', AdminEmployeeView.as_view({'delete': 'destroy'}), name='admin_employee_delete'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
